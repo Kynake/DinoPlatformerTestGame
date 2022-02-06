bdd605af968658c2b5d0518dd39e1529efc4bd37
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2022-02-05 23:09:22 -0300

    Implement Basic player sideways movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase, RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
 
+    private Rigidbody2D _rb;
+    private float _lateralMoveForce = 0;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        InputController.OnMove += Move;
+        InputController.OnJump += Jump;
+    }
+
+    private void OnDisable()
+    {
+        InputController.OnMove -= Move;
+        InputController.OnJump -= Jump;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    public void Move(Vector2 direction)
+    {
+        _lateralMoveForce = Vector2.Dot(direction, Vector2.right);
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    private void UpdateMovement()
+    {
+        _rb.velocity = new Vector2(_lateralMoveForce * Speed, _rb.velocity.y);
+        if(_lateralMoveForce != 0)
+        {
+            transform.localScale = new Vector3(_lateralMoveForce > 0? 1 : -1, 1, 1);
+        }
+    }
 }
