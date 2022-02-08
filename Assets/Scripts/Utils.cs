dd6faa2a58343296b7f3d3dc1729ac5fc1471ec0
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2022-02-07 22:10:57 -0300

    Create MainMenu

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Utils
+{
+    public static void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 % SceneManager.sceneCountInBuildSettings, LoadSceneMode.Single);
+    }
+
+    public static void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
